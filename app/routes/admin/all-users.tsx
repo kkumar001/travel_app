d55c1b36c81213ed084a976fe3c622a6035b8538
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-11-02 17:03:26 +0530

    header implemented

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,9 +1,16 @@
+import { Header } from "components"
+
 const AllUSers = () => {
-  return (
-    <div>
-      All Users
-    </div>
-  )
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title="Trips Page"
+                description="Check out our current users in real time"
+            />
+
+            All Users Page Contents
+        </main>
+    )
 }
 
 export default AllUSers
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,9 +1,21 @@
-const dashboard = () => {
-  return (
-    <div>
-      dashboard
-    </div>
-  )
+import { Header } from "components"
+
+const Dashboard = () => {
+    const user = {
+        name: 'Kailash Kumar',
+        email: 'kk@ss.com',
+        imageUrl: '/assets/images/david.webp'
+    }
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title={`Welcome ${user?.name ?? "Guest"} 👋`}
+                description="Track activity, trends and popular destinations in real time"
+            />
+
+            Dasboard Page Contents
+        </main>
+    )
 }
 
-export default dashboard
+export default Dashboard
